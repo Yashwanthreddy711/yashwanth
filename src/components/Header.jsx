77c6c0f176fa4434c5238785006d0426f824ff6c
yashwanth <yashwanthreddy7675@gmail.com>
2023-04-04 20:20:02 +0530

    sixth commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,10 +4,10 @@ import {HashLink} from 'react-router-hash-link';
 
 const Header = () => {
   return (
-    <div className="container h-25 min-w-max bg-custom p-5 drop-shadow-lg">
-    <nav className="flex items-center">
+    <div className=" h-23  bg-custom p-5 drop-shadow-lg flex-wrap ">
+    <nav className="flex w-full  items-center py-2 ">
   <img className="w-40 h-20 px-5 pt-3" src="../images/logo.png" alt="" />
-  <ul className="flex w-full justify-end">
+  <ul className="flex flex-wrap w-full justify-end ">
     {/* <li className="cursor-pointer inline-block px-10 text-white">Home</li>
     <li className="cursor-pointer inline-block px-10 text-white">
       About
@@ -19,11 +19,12 @@ const Header = () => {
       Contact
     </li>
      */}
-            <HashLink to = {"/#home"} className="cursor-pointer inline-block px-10  text-white">Home</HashLink>
-            <HashLink to = {"/#about"} className="cursor-pointer inline-block px-10 text-white">About</HashLink>
-            <Link to= {"/projects"} className="cursor-pointer inline-block px-10 text-white">Projects</Link>
-            <Link to={"/contact"} className="cursor-pointer inline-block px-10 text-white">Contact</Link>
-            <Link to={"/resume"} className="cursor-pointer inline-block px-10 text-white">Resume</Link>
+            <HashLink to = {"/#home"} className="cursor-pointer inline-block px-10 mt-2  text-white">Home</HashLink>
+            <HashLink to = {"/#about"} className="cursor-pointer inline-block px-10 mt-2 text-white">About</HashLink>
+            <Link to= {"/projects"} className="cursor-pointer inline-block px-10 mt-2 text-white">Projects</Link>
+            <Link to={"/contact"} className="cursor-pointer inline-block px-10 mt-2 text-white">Contact</Link>
+            <Link to={"/resume"} className="cursor-pointer  relative  text-primary bg-white  rounded-3xl mt-1 py-2 px-8 font-medium  mr-4  ">Resume</Link>
+            
   </ul>
 </nav>
 
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from "react";
+import { FaGithub, FaTwitter, FaLinkedin } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div className='  container min-h-screen min-h-full min-w-max bg-custom text-white'>Home</div>
-  )
-}
+    <div className="min-h-screen min-w-screen w-full flex bg-custom scrollbar-hide ">
+      <div className="min-h-screen w-20 bg-custom flex justify-center flex-col ">
+        <a href="https://github.com" className="text-white mt-10 px-5">
+          <FaGithub size={30} />
+        </a>
+        <a href="https://twitter.com" className="text-white mt-10 px-5">
+          <FaTwitter size={30} />
+        </a>
+        <a href="https://linkedin.com" className="text-white mt-10 px-5 ">
+          <FaLinkedin size={30} />
+        </a>
+      </div>
+      <div className="min-h-screen min-w-screen w-full flex flex-col  items-center bg-custom">
+
+           <h1 className="text-6xl font-bold text-white mt-20 ">HEY, I'M YASHWANTH</h1>
+           <h6 className="text-lg text-white flex justify-center mt-4  ">A Frontend focused Web Developer building the Frontend of Websites and Web Applications that leads to the success of the overall product
+</h6>0
+      </div>
+    </div>
+  );
+};
 
 export default Home;
